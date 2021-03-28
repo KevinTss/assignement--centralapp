0f0b71f30072d295c88b4f70ea877ca942188192
KevinTss <kevin.tassi@gmail.com>
2021-03-28 12:25:58 +0200

    Fix: Head was missing

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { StyledHeader, StyledLogoImage, StyledFooter } from "./style";
 import GlobalStyle from "../../styles/GlobalStyle";
 import logoUrl from "../../assets/logo.png";
+import Head from "../head";
 
 const Layout = ({ children }) => (
   <>
     <GlobalStyle />
+    <Head />
     <StyledHeader>
       <StyledLogoImage src={logoUrl} alt="finder logo" />
     </StyledHeader>
